ace61524cd1b2317827e6f7ec6f9181a13e709aa
prasannaj-15 <prasanna15j@gmail.com>
2023-03-29 17:23:47 +0530

    tit tac toe backend code

---
+++ Main.java
@@ -0,0 +1,106 @@
+import java.util.*;
+
+class Main{
+
+    public static void printBoard(char[][] board){
+
+        for(int row=0;row<board.length;row++){
+            for(int col=0;col<board[row].length;col++){
+                System.out.print(board[row][col]+" | ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static boolean haveWon(char[][] board, char player){
+
+        //check the rows
+        for(int row=0;row<board.length;row++){
+            if(board[row][0] == player && board[row][1] == player && board[row][2] == player){
+                return true;
+            }
+        }
+
+        //check the columns
+
+        for(int col=0;col<board[0].length;col++){
+            if(board[0][col] == player && board[1][col] == player && board[2][col] == player){
+                return true;
+            }
+        }
+
+        //check the diagonals
+
+            if(board[0][0] == player && board[1][1] == player && board[2][2] == player){
+                return true;
+            }
+        
+            if(board[0][2] == player && board[1][1] == player && board[2][0] == player){
+                return true;
+            }
+
+            return false;
+    }
+
+
+    public static void main(String args[]){
+
+        char[][] board = new char[3][3];
+
+        for(int row=0;row<board.length;row++){
+            for(int col=0;col<board[row].length;col++){
+                board[row][col] = ' ';
+            }
+        }
+
+        char player = 'X';
+        boolean gameOver = false;
+        int attempts = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(!gameOver){
+            printBoard(board);
+            System.out.println("player -  "+ player + " enter: ");
+            int row = scanner.nextInt();
+            int col = scanner.nextInt();
+
+            while(row > 2 || col > 2){
+                System.out.println("Enter valid positions");
+                row = scanner.nextInt();
+                col = scanner.nextInt();
+            }
+
+            if(board[row][col] == ' '){
+               board[row][col] = player;
+                attempts++;
+                if(attempts >= 9){
+                    printBoard(board);
+                    System.out.println("Oops its Tie!");
+                    return;
+                }
+
+
+                gameOver = haveWon(board, player);
+               
+               if(gameOver){
+                   printBoard(board);
+                System.out.println("Congratulations player "+ player +" has Won");
+               }
+               else{
+                    player = (player=='X') ? 'O' : 'X'; 
+               }
+            }
+            else{
+                    System.out.println("Invalid move, Try again!");
+
+            }
+
+        }
+
+
+
+    }
+
+}
